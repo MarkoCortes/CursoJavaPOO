804855a749243aa8ffe54128f854942cc570daae
MarkoCortes <bcortesmarko@gmail.com>
2021-07-08 20:25:40 -0500

    Se agrega constructor basico a Doctor

--- src/Doctor.java
+++ src/Doctor.java
@@ -3,6 +3,14 @@ public class Doctor {
     String name;
     String speciality;
 
+    Doctor(){
+        System.out.println("Construyendo el objeto Doctor");
+    }
+    Doctor(String name){
+        System.out.println("El nombre del Doctor es "+ name);
+        this.name = name;
+    }
+
     //Comportamiento
     public void showName(){
         System.out.println(this.name);
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Marko Alan");
         myDoctor.name= "Alejandro Rodriguez";
-
+        System.out.println(myDoctor.name);
     }
 }
